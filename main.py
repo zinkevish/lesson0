b45714ac38fce547cd61e196b681495919408fd2
zinkevish <zinkevish77@gmail.com>
2024-11-18 19:54:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print ("1st program")
+print ((9**0.5)*5)
+
+print ("2nd program")
+print ( 9.99>9.98 and 1000 != 1000.1)
+
+print ("3rd program")
+print (2*2+2)
+print (2*(2+2))
+print (2*2+2 == 2*(2+2))
+
+print ("4th program")
+print ( '123.456' )
+print (float ('123.456') *10)
+print (int (1234.456))
+print ( int ((1234.456) % 10))  # реально не понял как здесь % работает???
+
+
